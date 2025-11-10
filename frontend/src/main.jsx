eafdc80b002f851903217a2ed41b7a9165dd9d9d
jonathancode01 <icontatojonathan@gmail.com>
2025-11-10 00:02:49 -0400

    css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 // Em src/main.jsx (O JEITO CORRETO)
 
 import React from 'react';
+import './index.css';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App'; // 👈 Importa o seu App.jsx
